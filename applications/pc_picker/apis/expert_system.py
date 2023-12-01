e494bef6df458951f8898634dfe3647e3533c86b
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-01 18:49:12 +0200

    Edit expert system file

--- applications/pc_picker/apis/expert_system.py
+++ applications/pc_picker/apis/expert_system.py
@@ -2,17 +2,10 @@ from experta import *
 from ...dashboard import models as dashboard_models
 from ...dashboard.apis import serializers as dashboard_serializers
 
-class MyFact(Fact):
+class InputFact(Fact):
     pass
 
 class ExpertSystem(KnowledgeEngine):
-    @Rule(MyFact(price=500))
-    def pick_pc(self):
-        min_price = 300
-        max_price = 700
-        try:
-            motherboards = dashboard_models.Motherboard.objects.filter(price__range=(min_price, max_price))
-            serializer = dashboard_serializers.MotherboardSerializer(motherboards, many=True)
-            return serializer.data
-        except dashboard_models.Motherboard.DoesNotExist:
-            return []
+    @Rule(InputFact(field_id=1))
+    def pick_pc_for_programming(self):
+        return 'programming'
