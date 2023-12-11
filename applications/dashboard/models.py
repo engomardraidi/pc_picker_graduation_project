e348bb54a3001c5d9c01451b79b17d711ca175c4
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-11 13:32:41 +0200

    Add image_url field to Case model

--- applications/dashboard/models.py
+++ applications/dashboard/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -390,6 +391,10 @@ class Case(models.Model):
     color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     side_panel = models.ForeignKey(CaseSidePanel, on_delete=models.SET_NULL, null=True)
+    image_url = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
