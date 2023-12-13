98d06a2ec935f811e3251e971957e922c374eae3
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-13 16:08:46 +0200

    Test View

--- applications/pc_picker/apis/views.py
+++ applications/pc_picker/apis/views.py
@@ -1,20 +1,16 @@
-import pandas as pd
-import numpy as np
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .expert_system import ExpertSystem, InputFact
-from ...dashboard import models as models_dashboard
-from ...dashboard.apis import serializers  as serializers_dashboard
-from jinja2 import Template
+from .expert_system_folder.fields_knowledge import FieldsKnowledge
+from .expert_system_folder.input_fact import InputFact
 
 @api_view(['GET'])
 def test_expert_system(request):
-    # field_id = request.data.get('field_id', None)
-    # budget = request.data.get('budget', None)
+    field_id = request.data.get('field_id', None)
+    budget = request.data.get('budget', None)
 
-    # expert_system = ExpertSystem()
-    # expert_system.reset()
-    # expert_system.declare(InputFact(field_id=field_id, budget=budget))
-    # result = expert_system.run()
+    expert_system = FieldsKnowledge()
+    expert_system.reset()
+    expert_system.declare(InputFact(field_id=field_id, budget=budget))
+    result = expert_system.run()
 
-    return Response({'result': None})
+    return Response({'result': result})
