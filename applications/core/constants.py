41573131fbf3b69a29aa38de82b5993824ac5d9c
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-13 16:48:45 +0200

    constants variables

---
+++ applications/core/constants.py
@@ -0,0 +1,5 @@
+class Constants:
+    DETAIL_KEY = 'detail'
+    REQUEIRED_FIELDS = 'field and budget are required'
+    NOT_A_NUMBER = 'field and budget must be a number'
+    FIELD_NOT_EXIST = 'the field you want not exist'
