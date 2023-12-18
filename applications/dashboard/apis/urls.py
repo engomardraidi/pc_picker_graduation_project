fcb55d739b75354ee5148f9e937830ee3547bea9
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-18 17:26:46 +0200

    Create single-motherboard api

--- applications/dashboard/apis/urls.py
+++ applications/dashboard/apis/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('motherboards/', views.MotherboardListView.as_view(), name='motherboards'),
+    path('motherboards/', views.MotherboardListView.as_view(), name='list-motherboards'),
+    path('motherboards/<int:pk>/', views.SingleMotherboardView.as_view(), name='single-motherboard'),
 ]
--- applications/dashboard/apis/views.py
+++ applications/dashboard/apis/views.py
@@ -1,7 +1,17 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .. import models
 from . import serializers
+from rest_framework.response import Response
 
 class MotherboardListView(ListCreateAPIView):
-    queryset = models.Motherboard.get_objects()
+    queryset = models.Motherboard.filter_objects(status=True)
     serializer_class = serializers.MotherboardSerializer
+
+class SingleMotherboardView(RetrieveUpdateDestroyAPIView):
+    queryset = models.Motherboard.filter_objects(status=True)
+    serializer_class = serializers.MotherboardSerializer
+
+    def delete(self, request, *args, **kwargs):
+        request.data.update({'status': False})
+        self.patch(request, *args, **kwargs)
+        return Response(status=204)
