733955bd1c0b48e710de9bfe75dbe580ccbf72c9
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-19 11:38:43 +0200

    Create BaseViewSet

--- applications/dashboard/apis/views.py
+++ applications/dashboard/apis/views.py
@@ -4,20 +4,16 @@ from . import serializers
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-class MotherboardViewSet(ModelViewSet):
+class BaseViewSet(ModelViewSet):
+    def destroy(self, request, *args, **kwargs):
+        request.data.update({'status': False})
+        self.partial_update(request, *args, **kwargs)
+        return Response(status=204)
+
+class MotherboardViewSet(BaseViewSet):
     queryset = models.Motherboard.get_active_objects()
     serializer_class = serializers.MotherboardSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        request.data.update({'status': False})
-        self.partial_update(request, *args, **kwargs)
-        return Response(status=204)
-
-class CPUViewSet(ModelViewSet):
-    queryset = models.CPU.get_active_objects()
+class CPUViewSet(BaseViewSet):
+    queryset = models.CPU.get_objects()
     serializer_class = serializers.CPUSerializer
-
-    def destroy(self, request, *args, **kwargs):
-        request.data.update({'status': False})
-        self.partial_update(request, *args, **kwargs)
-        return Response(status=204)
