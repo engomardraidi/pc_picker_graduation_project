8bdb096888301db41d4a114daf3b86895321abbc
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-26 23:57:07 +0200

    Modify pick-pc api name

--- applications/pc_picker/apis/urls.py
+++ applications/pc_picker/apis/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('devices/', views.ListOfDevices.as_view(), name='devices'),
     path('fields/', views.ListOfFields.as_view(), name='fields'),
-    path('pick-pc/', views.pick_pc, name='pick_pc'),
+    path('pick-pc/', views.pick_pc, name='pick-pc'),
 ]
