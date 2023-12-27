8dd9f460169ae3b98ad6a162515583cac53da0bb
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-27 10:35:52 +0200

    Classification a new ram when saved it in DB

--- applications/dashboard/apis/signals.py
+++ applications/dashboard/apis/signals.py
@@ -77,7 +77,7 @@ def classification_cpu(sender, instance, created, **kwargs):
         new_predictions = classifier.predict(new_data)
 
         list_data = []
-        print(instance.id)
+
         if new_predictions[0, 0]:
             # gaming
             list_data.append({'cpu': instance.id, 'field': 3})
@@ -93,6 +93,74 @@ def classification_cpu(sender, instance, created, **kwargs):
 
         serializer = serializers.CPUFieldSerializer(data=list_data, many=True)
         if serializer.is_valid():
+            serializer.save()
+
+        # Display the predictions
+        print('Predicted classification for the new data:')
+        print('Gaming:', new_predictions[0, 0])
+        print('Graphic Design:', new_predictions[0, 1])
+        print('Programming:', new_predictions[0, 2])
+        print('Office:', new_predictions[0, 3])
+
+@receiver(post_save, sender=models.RAM)
+def classification_ram(sender, instance, created, **kwargs):
+    if created:
+        df = pd.read_csv("./././datasets/ram_fields_class.csv")
+
+        df = df.drop(columns=['timings', 'sticks', 'url', 'image_url', 'created_at', 'updated_at', 'status', 'producer'])
+
+        X = df[['size', 'clock', 'type', 'price']]
+
+        y = df[['gaming', 'graphic design', 'programming', 'office']]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Initialize the Random Forest classifier
+        classifier = MultiOutputClassifier(RandomForestClassifier(random_state=42))
+
+        # Train the model on the training data
+        classifier.fit(X_train, y_train)
+
+        # Make predictions on the test set
+        predictions = classifier.predict(X_test)
+
+        # Evaluate the accuracy for each label
+        for i, label in enumerate(y.columns):
+            accuracy = accuracy_score(y_test[label], predictions[:, i])
+            print(f'Accuracy for {label}: {accuracy}')
+
+        # Display overall classification report
+        print('\nOverall Classification Report:\n', classification_report(y_test, predictions))
+
+        print(instance.type.id)
+
+        new_data = pd.DataFrame({
+            'size': [instance.size],
+            'clock': [instance.clock],
+            'type': [instance.type.id],
+            'price': [instance.price]
+        })
+
+        # Make predictions for the new data
+        new_predictions = classifier.predict(new_data)
+
+        list_data = []
+
+        if new_predictions[0, 0]:
+            # gaming
+            list_data.append({'ram': instance.id, 'field': 3})
+        if new_predictions[0, 1]:
+            # graphic design
+            list_data.append({'ram': instance.id, 'field': 2})
+        if new_predictions[0, 2]:
+            # programming
+            list_data.append({'ram': instance.id, 'field': 1})
+        if new_predictions[0, 3]:
+            # office
+            list_data.append({'ram': instance.id, 'field': 4})
+
+        serializer = serializers.RAMFieldSerializer(data=list_data, many=True)
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
 
         # Display the predictions
