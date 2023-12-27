83ced3c47caf57cbf4e412777189dc546ee6445a
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-27 12:34:36 +0200

    Remove test api

--- applications/dashboard/apis/urls.py
+++ applications/dashboard/apis/urls.py
@@ -11,4 +11,4 @@ router.register(r'cases', views.CaseViewSet, basename='cases')
 router.register(r'internal-drives', views.InternalDrivesViewSet, basename='internal-drives')
 router.register(r'power-supplies', views.PowerSupplyViewSet, basename='power-supplies')
 
-urlpatterns = router.urls + [path('test/', views.test)]
+urlpatterns = router.urls
