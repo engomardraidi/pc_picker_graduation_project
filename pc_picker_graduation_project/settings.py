0594906feb8400c3d718afaecfb35782bff722e8
EngOmarDraidi <omar.m3z2001@gmail.com>
2023-12-29 16:41:33 +0200

    Change images path

--- pc_picker_graduation_project/settings.py
+++ pc_picker_graduation_project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
