1c96f5532af7540ee5c55e7110c7d161d6053252
EngOmarDraidi <omar.m3z2001@gmail.com>
2024-01-10 10:46:14 +0200

    Change DB from sqlite3 to PostgreSQL

--- pc_picker_graduation_project/settings.py
+++ pc_picker_graduation_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'pc_picker_graduation_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'graduation_project_pc_picker',
+        'USER': 'postgres',
+        'PASSWORD': '3mar3mar',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
