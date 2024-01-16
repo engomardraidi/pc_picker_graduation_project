e9138ad0d97acbe49d9359eff7c0cfea2cb9702a
EngOmarDraidi <omar.m3z2001@gmail.com>
2024-01-16 10:11:44 +0200

    Modify mobiles knowledge to pick mobiles for more one field

--- applications/pc_picker/apis/expert_system/mobiles_knowledge.py
+++ applications/pc_picker/apis/expert_system/mobiles_knowledge.py
@@ -8,6 +8,7 @@ import decimal
 
 class MobilesKnowledge(ExpertSystem):
     def __get_mobiles(self, field_id, budget):
+        print(field_id)
         perc = decimal.Decimal(0.03)
         mobiles = []
 
@@ -15,15 +16,15 @@ class MobilesKnowledge(ExpertSystem):
             perc += decimal.Decimal(0.03)
             min_budget = budget - (budget * perc)
             max_budget = budget + (budget * perc)
-            mobiles = Mobile.filter_objects(mobileuse__use__id=field_id, price__range=(min_budget, max_budget))
+            mobiles = Mobile.filter_objects(mobileuse__use__id__in=field_id, price__range=(min_budget, max_budget))
 
         return mobiles
 
-    @Rule(AS.rule << InputFact(field_id=1))
+    @Rule(AS.rule << InputFact(field_id=[1]))
     def pick_mobile_for_performance_use(self, rule):
         budget = decimal.Decimal(rule['budget'])
         
-        list_mobiles = self.__get_mobiles(1, budget)
+        list_mobiles = self.__get_mobiles([1], budget)
         mobiles = []
 
         for mobile in list_mobiles:
@@ -31,11 +32,11 @@ class MobilesKnowledge(ExpertSystem):
 
         return mobiles
 
-    @Rule(AS.rule << InputFact(field_id=2))
+    @Rule(AS.rule << InputFact(field_id=[2]))
     def pick_mobile_for_camera_use(self, rule):
         budget = decimal.Decimal(rule['budget'])
         
-        list_mobiles = self.__get_mobiles(2, budget)
+        list_mobiles = self.__get_mobiles([2], budget)
         mobiles = []
 
         for mobile in list_mobiles:
@@ -43,11 +44,11 @@ class MobilesKnowledge(ExpertSystem):
 
         return mobiles
 
-    @Rule(AS.rule << InputFact(field_id=3))
+    @Rule(AS.rule << InputFact(field_id=[3]))
     def pick_mobile_for_battery_use(self, rule):
         budget = decimal.Decimal(rule['budget'])
         
-        list_mobiles = self.__get_mobiles(3, budget)
+        list_mobiles = self.__get_mobiles([3], budget)
         mobiles = []
 
         for mobile in list_mobiles:
@@ -55,11 +56,95 @@ class MobilesKnowledge(ExpertSystem):
 
         return mobiles
 
-    @Rule(AS.rule << InputFact(field_id=4))
+    @Rule(AS.rule << InputFact(field_id=[4]))
     def pick_mobile_for_browsing_use(self, rule):
         budget = decimal.Decimal(rule['budget'])
         
-        list_mobiles = self.__get_mobiles(4, budget)
+        list_mobiles = self.__get_mobiles([4], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[1,2]))
+    def pick_mobile_for_performance_and_camera_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([1,2], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[1,3]))
+    def pick_mobile_for_performance_and_battery_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([1,3], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[2,3]))
+    def pick_mobile_for_camera_and_battery_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([2,3], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[2,4]))
+    def pick_mobile_for_camera_and_browsing_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([2,4], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[3,4]))
+    def pick_mobile_for_battery_and_browsing_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([3,4], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[1,2,3]))
+    def pick_mobile_for_perfrormance_and_camera_and_battery_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([1,2,3], budget)
+        mobiles = []
+
+        for mobile in list_mobiles:
+            mobiles.append(get_mobile_as_json(mobile, budget))
+
+        return mobiles
+
+    @Rule(AS.rule << InputFact(field_id=[2,3,4]))
+    def pick_mobile_for_camera_and_battery_and_browsing_use(self, rule):
+        budget = decimal.Decimal(rule['budget'])
+        
+        list_mobiles = self.__get_mobiles([2,3,4], budget)
         mobiles = []
 
         for mobile in list_mobiles:
