f4b09d4eaa9651b4652f7d956e7d6350d2d6a7fc
EngOmarDraidi <omar.m3z2001@gmail.com>
2024-01-16 10:12:09 +0200

    Add new constants

--- applications/core/constants.py
+++ applications/core/constants.py
@@ -2,6 +2,8 @@ class Constants:
     DETAIL_KEY = 'detail'
     REQUEIRED_FIELDS = 'Field and budget are required'
     NOT_A_NUMBER = 'Field and budget must be a number'
+    FIELD_NOT_A_LIST = 'Field must be a list of numbers'
+    BUDGET_NOT_A_NUMBER = 'Budget must be a number'
     FIELD_NOT_EXIST = 'The field you want not exist'
     LOGIN_INFORMATION_REQUIRED = 'Username and password are required'
     LOGIN_INFORMATION_INCORRECT = 'Username or password is incorrect'
